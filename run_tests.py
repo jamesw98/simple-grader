a070ef5faf7d89bea2a0d9107754490da6acf181
jamesw98 <xeekatar@gmail.com>
2021-04-07 18:34:38 -0400

    added java support

--- run_tests.py
+++ run_tests.py
@@ -5,7 +5,11 @@ from read_json import print_test_info
 import subprocess as sp
 import os
 
+# grades a student program
 def grade(grading_json_filename, prog_name):
+    compiled = False
+    compiler = []
+
     # makes sure the grading data is valid
     if (load_grading_data(grading_json_filename) == None):      
         return
@@ -14,61 +18,88 @@ def grade(grading_json_filename, prog_name):
     if (not check_extension(prog_name)):
         return
 
+    if (".zip" in prog_name):
+        # decompress, put into folder
+        pass
+    elif (".tar" in prog_name):
+        # decompress, put into folder
+        pass    
+    
+    # haskell
+    if (".hs" in prog_name):
+        compiled = True
+        compiler.append("ghc")
+    # c
+    elif (".c" in prog_name):
+        compiled = True
+        compiler.append("gcc")
+    # java
+    elif (".java" in prog_name):
+        compiled = True
+        compiler.append("javac")
+    
+    # if the program needs to be compiled, compile it
+    if (compiled):
+        # TODO make this a function
+        if (".java" in prog_name):
+            sp.run(compiler + [prog_name], check=True, universal_newlines=True, stdout=sp.PIPE, stderr=sp.PIPE)
+        elif (".c" in prog_name or ".hs" in prog_name):
+            sp.run(compiler + ["-o", "student_exe", prog_name], check=True, universal_newlines=True, stdout=sp.PIPE, stderr=sp.PIPE)
+
     # prints the test info
     total_points = print_test_info()
-
     total_score = 0
 
     # runs tests and displays results
     for test in get_all_tests():
-        total_score += run_test(test, prog_name)
+        total_score += run_test(test, prog_name, compiled)
 
+    # if the langauge is compiled, remove the executable
+    if (compiled):
+        # TODO make this a function
+        if (".java" in prog_name):
+            os.remove(prog_name.replace(".java", "") + ".class")
+        elif (".c" in prog_name or ".hs" in prog_name):
+            os.remove("student_exe")
+
+    # display over all results
     print("\n" + "=" * 10 + " Results of All Tests " + "=" * 10)
     print(f"\nYour score {total_score}/{total_points}")
     print("Your percent " + str(round(calc_percent(total_score, total_points), 2)) + "%")
 
 # checks if program has valid extension
 def check_extension(prog_name) -> bool:
-    if (".py" not in prog_name and ".hs" not in prog_name and ".c" not in prog_name):
+    if (".py" not in prog_name and ".hs" not in prog_name and ".c" not in prog_name 
+        and ".java" not in prog_name and ".zip" not in prog_name and ".tar" not in prog_name):
         print("Invalid program type, currently only Python and Haskell are supported")
         return False
 
     return True
 
 # runs tests
-def run_test(test, prog_name):
+def run_test(test, prog_name, compiled):
     print("\n" + "=" * 10 + f" Running test: {test.name} " + "=" * 10)
 
     # renames variables to make it easier to read
     points = test.points
     points_off = test.points_off_per_line
     max_points = test.max_points_off
-
     test_expected = test.expected_output_file
 
     try:
-        compiled = False
-        compiler = []
-
         # TODO compiler flags
         # TODO compressed (tar, zip)
-        # checks for python or haskell
+
+        # runs python program
         if (".py" in prog_name):
             student_exe = sp.run(["python3", prog_name] + test.arguments, check=True, universal_newlines=True, stdout=sp.PIPE, stderr=sp.PIPE)
-        # compiles and runs the haskell program
-        elif (".hs" in prog_name):
-            compiled = True
-            compiler.append("ghc")
-        # compiles and runs the c program 
-        elif (".c" in prog_name):
-            compiled = True
-            compiler.append("gcc")
-
+    
         # compiles and runs a compiled language (c, haskell)
-        if (compiled):
-            sp.run(compiler + ["-o", "student_exe", prog_name], check=True, universal_newlines=True, stdout=sp.PIPE, stderr=sp.PIPE)
-            student_exe = sp.run(["./student_exe"] + test.arguments, check=True, universal_newlines=True, stdout=sp.PIPE, stderr=sp.PIPE)
-            os.remove("student_exe")
+        elif (compiled):
+            if (".java" in prog_name):
+                student_exe = sp.run(["java", prog_name.replace(".java", "")] + test.arguments, check=True, universal_newlines=True, stdout=sp.PIPE, stderr=sp.PIPE)
+            elif (".c" in prog_name or ".hs" in prog_name):
+                student_exe = sp.run(["./student_exe"] + test.arguments, check=True, universal_newlines=True, stdout=sp.PIPE, stderr=sp.PIPE)
 
     # student program crashed, or failed to compile
     except Exception as e:
@@ -132,3 +163,4 @@ def print_error(num, expected, received, points_off):
 # compares a student line to an expected line
 def check_line(student_line, expected_line):
     return student_line.lower().replace("\n", "") == expected_line.lower().replace("\n", "")
+    
