d02b0c2f784a8edc67ff72c4d9ec1ebd0f0d2d0c
jamesw98 <xeekatar@gmail.com>
2021-04-23 14:55:48 -0400

    changing directory file result naming convention

--- grader.py
+++ grader.py
@@ -5,7 +5,6 @@ from os.path import isfile
 
 from run_tests import grade
 
-# TODO run on dir
 # TODO more languages
 
 if len(sys.argv) < 2:
@@ -16,12 +15,17 @@ elif (sys.argv[1] == "-help"):
 else:
     # case for wanting to grade all files in a dir
     if (len(sys.argv) > 3 and sys.argv[2] == "-d"):
-        for f in listdir(sys.argv[3]):
-            if (isfile(f)):
-                print("Grading: " + f)
-                grade(sys.argv[1], f, sys.argv[3] + f + ".txt")
+        # loops through everything in a directory
+        for _file in listdir(sys.argv[3]):
+            if (isfile(_file) and ".txt" not in _file):
+                print(f"Grading: {_file}, outputting to: {sys.argv[3]}{_file}.txt")
+                grade(sys.argv[1], _file, sys.argv[3] + _file.split(".")[0] + ".result.txt")
+
     # user wants to write to a file instead of stdout
     elif (len(sys.argv) == 4 and sys.argv[1] == "-f"):
+        print(f"Grading: {sys.argv[3]}, outputting to: {sys.argv[3]}.txt")
         grade(sys.argv[2], sys.argv[3], sys.argv[3] + ".txt")
+
+    # standard case, grade one file, output to stdout
     else:
         grade(sys.argv[1], sys.argv[2])
