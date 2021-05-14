475a8de095c2bb8e3ac4a834060ca93f46385781
jamesw98 <xeekatar@gmail.com>
2021-05-13 20:05:57 -0400

    changed order of args

--- grader.py
+++ grader.py
@@ -11,26 +11,26 @@ from run_tests import grade
 if len(sys.argv) < 2:
     print("Invalid usage: ./grader.py -help for info")
 elif (sys.argv[1] == "-help"):
-    print("Usage: ./grader.py <json file> <student source code>")
+    print("Usage: ./grader.py <student source code> <json file>")
     print("For more info: https://github.com/jamesw98/simple-grader")
 else:
     # case for wanting to grade all files in a dir
-    if (len(sys.argv) > 3 and sys.argv[2] == "-d"):
+    if (len(sys.argv) > 3 and sys.argv[1] == "-d"):
         # loops through everything in a directory
-        if (not isdir(sys.argv[3])):
-            print(f"ERROR: {sys.argv[3]} is not a directory, please try again.")
+        if (not isdir(sys.argv[2])):
+            print(f"ERROR: {sys.argv[2]} is not a directory, please try again.")
         else:
-            for _file in listdir(sys.argv[3]):
+            for _file in listdir(sys.argv[2]):
                 if (isfile(_file) and ".txt" not in _file):
-                    print(f"Grading: {_file}, outputting to: {sys.argv[3]}{_file.split('.')[0]}.result.txt")
-                    grade(sys.argv[1], _file, sys.argv[3] + _file.split(".")[0] + ".result.txt")
+                    print(f"Grading: {_file}, outputting to: {sys.argv[2]}{_file.split('.')[0]}.result.txt")
+                    grade(sys.argv[3], _file, sys.argv[2] + _file.split(".")[0] + ".result.txt")
 
     # user wants to write to a file instead of stdout
     elif (len(sys.argv) == 4 and sys.argv[1] == "-f"):
-        print(f"Grading: {sys.argv[3]}, outputting to: {sys.argv[3].split('.')[0]}.result.txt")
-        grade(sys.argv[2], sys.argv[3], sys.argv[3].split(".")[0] + "result.txt")
+        print(f"Grading: {sys.argv[3]}, outputting to: {sys.argv[2].split('.')[0]}.result.txt")
+        grade(sys.argv[3], sys.argv[2], sys.argv[2].split(".")[0] + ".result.txt")
 
     # standard case, grade one file, output to stdout
     else:
-        grade(sys.argv[1], sys.argv[2])
+        grade(sys.argv[2], sys.argv[1])
         
